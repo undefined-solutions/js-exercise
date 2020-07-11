918d98a14e4efbd26bb590a4f2a34f1f337f1688
Jonathon Munsell <munsellj@gmail.com>
2020-07-11 14:33:47 -0700

    fix: rework 04-count for compatibility with jest-lite and code sandbox

--- src/04-count.test.js
+++ src/04-count.test.js
@@ -2,22 +2,32 @@ import { count } from './04-count';
 
 describe('counter', () => {
   const logFn = jest.fn();
+
   afterEach(() => {
     logFn.mockClear();
   });
-  it('should count from start number to end number, one per 1/10th of a second', () => {
+
+  it('should count from start number to end number, one per 1/10th of a second', (done) => {
     count(logFn, 1, 5);
     setTimeout(() => {
       expect(logFn).toHaveBeenCalledTimes(5);
-      expect(logFn).toHaveBeenNthCalledWith(1, 1);
-      expect(logFn).toHaveBeenNthCalledWith(5, 5);
+      // expect(logFn).toHaveBeenNthCalledWith(1, 1);
+      expect(logFn.mock.calls[0][0]).toBe(1);
+      // expect(logFn).toHaveBeenNthCalledWith(5, 5);
+      expect(logFn.mock.calls[4][0]).toBe(5);
+      done();
     }, 550);
   });
-  it('should provide a method to cancel the counting', () => {
+
+  it('should provide a method to cancel the counting', (done) => {
     const counter = count(logFn, 1, 5);
-    counter.cancel();
+    setTimeout(counter.cancel, 150);
     setTimeout(() => {
-      expect(logFn.mock.calls.length).toBeLessThan(5);
+      // expect(logFn.mock.calls.length).toBeGreaterThan(1);
+      expect(logFn.mock.calls.length > 1).toBeTruthy();
+      // expect(logFn.mock.calls.length).toBeLessThan(5);
+      expect(logFn.mock.calls.length < 5).toBeTruthy();
+      done();
     }, 750);
   });
-});
+}, 1000);
